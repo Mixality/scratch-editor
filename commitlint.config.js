3c48214df93ebe92de3033f2279526620003fdad
Maite-Aileen Brandt <maiteaileen@gmail.com>
2025-11-11 22:33:26 +0100

    Update commitlint config for Windows compatibility
    Modified the commitlint hook and configuration to be more lenient on Windows systems. The commit-msg hook now always succeeds if commitlint fails, and the config ignores all messages on Windows for improved compatibility.

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,12 +2,7 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     ignores: [
         message => message.startsWith('chore(release):'),
-        message => message.length < 10, // Ignore very short messages
-        message => true // Ignore all messages (effectively disable)
-    ],
-    rules: {
-        'type-enum': [0], // Disable type requirement
-        'subject-min-length': [0], // Disable minimum length
-        'header-max-length': [0], // Disable max length
-    }
+        // Windows compatibility: be more lenient
+        message => process.platform === 'win32'
+    ]
 };
